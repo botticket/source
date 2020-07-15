09b0dabfc7907b412f36e3d673810dcca124de84
botticket <59758461+botticket@users.noreply.github.com>
2020-07-15 19:21:30 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -359,7 +359,7 @@ def handle_message(event):
                     alert4 = 'Alert : อย่าเพิ่งเข้า'
                     alert5 = 'Alert : Vol น้อย'
 
-                    text = '\n' + text_request +'\n' + 'B: {} + 2 ช่อง'.format(OpenQ) + '\n' + 'Y: {} | M: {}'.format(OpenY,OpenM) + '\n' + 'H: {} | L: {}'.format(max_valueQ,min_value) + 'margin {}'.format(mValue) + '\n' + 'graphinfo : '+ send_url
+                    text = '\n' + text_request +'\n' + 'B: {} + 2 ช่อง'.format(OpenQ) + '\n' + 'Y: {} | M: {}'.format(OpenY,OpenM) + '\n' + 'H: {} | L: {}'.format(max_valueQ,min_value) + '\n' + 'margin {}'.format(mValue) + '\n' + 'graphinfo : '+ send_url
 
                     if float(value) > 7500000:
                         if  barY >= 0.00:
