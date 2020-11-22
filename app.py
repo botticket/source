6cc924cb155dd5b251b8f463a4b209cc67640ee7
botticket <59758461+botticket@users.noreply.github.com>
2020-11-22 19:06:37 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -83,209 +83,212 @@ def handle_message(event):
     print(request_text)
     linechat(request_text)
 
-    #try:    
-    if 'สวัสดี' in text_from_user:    
-        text_list = [
-            'สวัสดีจ้า คุณ {} '.format(disname),
-            'สวัสดีจ้า คุณ {} วันนี้จะเล่นตัวไหนดี'.format(disname),
-        ]
+    try:    
+        if 'สวัสดี' in text_from_user:    
+            text_list = [
+                'สวัสดีจ้า คุณ {} '.format(disname),
+                'สวัสดีจ้า คุณ {} วันนี้จะเล่นตัวไหนดี'.format(disname),
+            ]
 
-        from random import choice
-        word_to_reply = choice(text_list)
-        text_to_reply = TextSendMessage(text = word_to_reply)
-        line_bot_api.reply_message(
-                event.reply_token,
-                messages=[text_to_reply]
-            )
-        return 'OK'
-    elif 'hi' in text_from_user:    
-        text_list = [
-            'Hello {} '.format(disname),
-            'Good day {} '.format(disname),
-        ]
+            from random import choice
+            word_to_reply = choice(text_list)
+            text_to_reply = TextSendMessage(text = word_to_reply)
+            line_bot_api.reply_message(
+                    event.reply_token,
+                    messages=[text_to_reply]
+                )
+            return 'OK'
+        elif 'hi' in text_from_user:    
+            text_list = [
+                'Hello {} '.format(disname),
+                'Good day {} '.format(disname),
+            ]
 
-        from random import choice
-        word_to_reply = choice(text_list)
-        text_to_reply = TextSendMessage(text = word_to_reply)
-        line_bot_api.reply_message(
-                event.reply_token,
-                messages=[text_to_reply]
-            )
-        return 'OK'
-    else:
-                        
-        import math
-        import warnings
-        import numpy as np
-        import pandas as pd 
-        import matplotlib.pyplot as plt
-        warnings.filterwarnings("ignore")
-        from pandas_datareader import data 
-        from datetime import datetime,date
-        from bs4 import BeautifulSoup as soup 
-        from urllib.request import urlopen as req
-        from urllib.request import Request,urlopen
-        from openpyxl import Workbook, load_workbook
-        from line_notify import LineNotify
-        from scipy.stats import linregress
-        import matplotlib.dates as mdates
-
-        code = text_from_user
-        ticket = [text_from_user]
-        symbols = list(map(lambda e: e + '.bk', ticket))
-
-        def checkmarket(code):
-            url = 'https://www.settrade.com/C04_01_stock_quote_p1.jsp?txtSymbol={}&ssoPageId=9&selectPage=1'.format(code)
-            webopen = req(url)
-            page_html = webopen.read()
-            webopen.close()
-
-            data = soup(page_html, 'html.parser')
-            price = data.findAll('div',{'class':'col-xs-6'})
-            title = price[0].text
-            stockprice = price[2].text
-            stockprice = stockprice.replace('\n','')
-            change = price[3].text
-            change = change.replace('\n','')
-            change = change.replace('\r','')
-            change = change[87:]	
-            pbv = data.findAll('div',{'class':'text-right col-xs-4'})
-            pbv_rate = pbv[4].text
-            return [title,stockprice,change,pbv_rate]
-
-        st = checkmarket(code)
-        text_request = 'ตอนนี้ {} ราคา {} ({})'.format(st[0], st[1], st[2])
-
-        class stock:
-            def __init__(self,stock):
-                self.stock = stock
-
-            def ticket(self):
-                end = datetime.now()
-                start = datetime(end.year,end.month,end.day)
-                list = self.stock
-
-                dfY = data.DataReader(f'{list}', data_source="yahoo", start=yearly, end=end)
-                dfQ = data.DataReader(f'{list}', data_source="yahoo", start=quarter, end=end)
-                dfM = data.DataReader(f'{list}', data_source="yahoo", start=monthly, end=end)
-                list = list.replace('.bk','')
+            from random import choice
+            word_to_reply = choice(text_list)
+            text_to_reply = TextSendMessage(text = word_to_reply)
+            line_bot_api.reply_message(
+                    event.reply_token,
+                    messages=[text_to_reply]
+                )
+            return 'OK'
+        else:
                             
-                OpenY = dfY['Open'].iloc[0]
-                OpenY  = '%.2f'%OpenY
-                OpenY = str(OpenY)
+            import math
+            import warnings
+            import numpy as np
+            import pandas as pd 
+            import matplotlib.pyplot as plt
+            warnings.filterwarnings("ignore")
+            from pandas_datareader import data 
+            from datetime import datetime,date
+            from bs4 import BeautifulSoup as soup 
+            from urllib.request import urlopen as req
+            from urllib.request import Request,urlopen
+            from openpyxl import Workbook, load_workbook
+            from line_notify import LineNotify
+            from scipy.stats import linregress
+            import matplotlib.dates as mdates
 
-                OpenQ = dfQ['Open'].iloc[0]
-                OpenQ  = '%.2f'%OpenQ
-                OpenQ = str(OpenQ)
+            code = text_from_user
+            ticket = [text_from_user]
+            symbols = list(map(lambda e: e + '.bk', ticket))
 
-                OpenM = dfM['Open'].iloc[0]
-                OpenM  = '%.2f'%OpenM
-                OpenM = str(OpenM)
+            def checkmarket(code):
+                url = 'https://www.settrade.com/C04_01_stock_quote_p1.jsp?txtSymbol={}&ssoPageId=9&selectPage=1'.format(code)
+                webopen = req(url)
+                page_html = webopen.read()
+                webopen.close()
 
-                try:
-                    Close = float(st[1])
-                except ValueError:
-                    Close = dfY['Close'].iloc[-1]
+                data = soup(page_html, 'html.parser')
+                price = data.findAll('div',{'class':'col-xs-6'})
+                title = price[0].text
+                stockprice = price[2].text
+                stockprice = stockprice.replace('\n','')
+                change = price[3].text
+                change = change.replace('\n','')
+                change = change.replace('\r','')
+                change = change[87:]	
+                pbv = data.findAll('div',{'class':'text-right col-xs-4'})
+                pbv_rate = pbv[4].text
+                pbv_rate = pbv_rate.replace('\n','')
+                pbv_rate = pbv_rate.replace(' ','')
+                return [title,stockprice,change,pbv_rate]
 
-                Close  = '%.2f'%Close
-                Close = str(Close) 
+            st = checkmarket(code)
+            text_request = 'ตอนนี้ {} : {} ({})'.format(st[0], st[1], st[2])
 
-                barM = ((float(Close) - float(OpenM)) / float(OpenM) )*100
-                barM = '%.2f'%barM
-                barM = float(barM)
+            class stock:
+                def __init__(self,stock):
+                    self.stock = stock
 
-                Volume1 = dfY['Volume'].iloc[-1]
-                Volume2 = dfY['Volume'].iloc[-2]
+                def ticket(self):
+                    end = datetime.now()
+                    start = datetime(end.year,end.month,end.day)
+                    list = self.stock
 
-                Volume = (float(Volume1)+float(Volume2))/2
-                Volume  = '%.0f'%Volume
-                Volume = str(Volume)
+                    dfY = data.DataReader(f'{list}', data_source="yahoo", start=yearly, end=end)
+                    dfQ = data.DataReader(f'{list}', data_source="yahoo", start=quarter, end=end)
+                    dfM = data.DataReader(f'{list}', data_source="yahoo", start=monthly, end=end)
+                    list = list.replace('.bk','')
+                                
+                    OpenY = dfY['Open'].iloc[0]
+                    OpenY  = '%.2f'%OpenY
+                    OpenY = str(OpenY)
 
-                value = float(Volume) * float(Close)
-                value  = '%.2f'%value
-                value = str(value)
+                    OpenQ = dfQ['Open'].iloc[0]
+                    OpenQ  = '%.2f'%OpenQ
+                    OpenQ = str(OpenQ)
 
-                request_val = float(value) 
-                request_val  = '{:,.0f}'.format(request_val)
-                request_val = str(request_val)
-                
-                dfQ['mValue'] = (dfQ['Close'] - dfQ['Open']) * dfQ['Volume']
+                    OpenM = dfM['Open'].iloc[0]
+                    OpenM  = '%.2f'%OpenM
+                    OpenM = str(OpenM)
 
-                mValue = dfQ['mValue'].iloc[-1]
-                mValue = int(mValue)
+                    try:
+                        Close = float(st[1])
+                    except ValueError:
+                        Close = dfY['Close'].iloc[-1]
 
-                exit1 = float(OpenM) * 1.15
-                exit1 = '%.2f'%exit1
-                exit1 = str(exit1)
+                    Close  = '%.2f'%Close
+                    Close = str(Close) 
 
-                exit2 = float(OpenM) * 1.30
-                exit2 = '%.2f'%exit2
-                exit2 = str(exit2)
+                    barM = ((float(Close) - float(OpenM)) / float(OpenM) )*100
+                    barM = '%.2f'%barM
+                    barM = float(barM)
 
-                exit3 = float(OpenM) * 1.45
-                exit3 = '%.2f'%exit3
-                exit3 = str(exit3)
+                    Volume1 = dfY['Volume'].iloc[-1]
+                    Volume2 = dfY['Volume'].iloc[-2]
 
-                buyQ = float(OpenM) * 1.02
-                buyQ = '%.2f'%buyQ
-                buyQ = str(buyQ) 
+                    Volume = (float(Volume1)+float(Volume2))/2
+                    Volume  = '%.0f'%Volume
+                    Volume = str(Volume)
 
-                stopQ = float(OpenQ) * 0.98
-                stopQ = '%.2f'%stopQ
-                stopQ = str(stopQ) 
+                    value = float(Volume) * float(Close)
+                    value  = '%.2f'%value
+                    value = str(value)
 
-                buyY = float(OpenY) * 1.02
-                buyY = '%.2f'%buyY
-                buyY = str(buyY) 
+                    request_val = float(value) 
+                    request_val  = '{:,.0f}'.format(request_val)
+                    request_val = str(request_val)
+                    
+                    dfQ['mValue'] = (dfQ['Close'] - dfQ['Open']) * dfQ['Volume']
 
-                stopY = float(OpenY) * 0.98
-                stopY = '%.2f'%stopY
-                stopY = str(stopY) 
+                    mValue = dfQ['mValue'].iloc[-1]
+                    mValue = int(mValue)
 
-                max_valueQ = dfQ.nlargest(1, columns = 'High')
-                max_valueQ = max_valueQ['High'].iloc[0]
-                max_valueQ = '%.2f'%max_valueQ
-                max_valueQ = str(max_valueQ) 
+                    exit1 = float(OpenM) * 1.15
+                    exit1 = '%.2f'%exit1
+                    exit1 = str(exit1)
 
-                min_value = dfQ.nsmallest(1, columns = 'Low')
-                min_value = min_value['Low'].iloc[0]
-                min_value = '%.2f'%min_value
-                min_value = str(min_value) 
+                    exit2 = float(OpenM) * 1.30
+                    exit2 = '%.2f'%exit2
+                    exit2 = str(exit2)
 
-                alert1 = 'Alert : หุ้นวิ่งเมื่อเลย {}'.format(OpenM)
-                alert2 = 'Alert : หุ้นกำลังย่อ รอ {}'.format(OpenM)
-                alert3 = 'Alert : หุ้นลงต่อเมื่อหลุด {}'.format(OpenM)
-                
-                text = '\n' + text_request + '\n' + 'แนวต้าน {} | แนวรับ {}'.format(max_valueQ,min_value)
+                    exit3 = float(OpenM) * 1.45
+                    exit3 = '%.2f'%exit3
+                    exit3 = str(exit3)
 
-                if barM > 0.00:
-                    if float(mValue) > 0:
-                        word_to_reply = str(alert1 + text)
+                    buyQ = float(OpenM) * 1.02
+                    buyQ = '%.2f'%buyQ
+                    buyQ = str(buyQ) 
+
+                    stopQ = float(OpenQ) * 0.98
+                    stopQ = '%.2f'%stopQ
+                    stopQ = str(stopQ) 
+
+                    buyY = float(OpenY) * 1.02
+                    buyY = '%.2f'%buyY
+                    buyY = str(buyY) 
+
+                    stopY = float(OpenY) * 0.98
+                    stopY = '%.2f'%stopY
+                    stopY = str(stopY) 
+
+                    max_valueQ = dfQ.nlargest(1, columns = 'High')
+                    max_valueQ = max_valueQ['High'].iloc[0]
+                    max_valueQ = '%.2f'%max_valueQ
+                    max_valueQ = str(max_valueQ) 
+
+                    min_value = dfQ.nsmallest(1, columns = 'Low')
+                    min_value = min_value['Low'].iloc[0]
+                    min_value = '%.2f'%min_value
+                    min_value = str(min_value) 
+
+                    alert1 = 'เลย {} ซื้อติดมือ'.format(OpenM)
+                    alert2 = 'กำลังย่อ / ไม่หลุด {} ห่อกลับ'.format(OpenM)
+                    alert3 = 'หลุด {} ลงต่อ'.format(OpenM)
+                    
+                    
+                    text = text_request + '\n' + 'แนวต้าน {} | แนวรับ {}'.format(max_valueQ,min_value) + '\n' 
+
+                    if float(mValue) > 0.00:
+                        if  barM > 0:
+                            word_to_reply = str( text + alert1 )
+                        else:
+                            word_to_reply = str( text + alert3 )
                     else:
-                        word_to_reply = str(alert2 + text)
-                else:
-                    word_to_reply = str(alert3 + text)
+                        word_to_reply = str( text + alert2 )
 
-                text_to_reply = TextSendMessage(text = word_to_reply)
-                line_bot_api.reply_message(
-                        event.reply_token,
-                        messages=[text_to_reply])
-                linechat(word_to_reply)
-                
-        for symbol in symbols:
-            stock(symbol).ticket()
-    #except:
-    #    text_list = [
-    #        '{} ไม่มีในฐานข้อมูล {} ลองใหม่อีกครั้ง'.format(text_from_user,disname),
-    #        '{} พิมพ์ชื่อหุ้น {} ไม่ถูกต้อง ลองใหม่อีกครั้ง'.format(disname, text_from_user)]
+                    text_to_reply = TextSendMessage(text = word_to_reply)
+                    line_bot_api.reply_message(
+                            event.reply_token,
+                            messages=[text_to_reply])
+                    linechat(word_to_reply)
+                    
+            for symbol in symbols:
+                stock(symbol).ticket()
+    except:
+        text_list = [
+            '{} ไม่มีในฐานข้อมูล {} ลองใหม่อีกครั้ง'.format(text_from_user,disname),
+            '{} พิมพ์ชื่อหุ้น {} ไม่ถูกต้อง ลองใหม่อีกครั้ง'.format(disname, text_from_user)]
 
-    #    from random import choice
-    #    word_to_reply = choice(text_list)        
-    #    text_to_reply = TextSendMessage(text = word_to_reply)
+        from random import choice
+        word_to_reply = choice(text_list)        
+        text_to_reply = TextSendMessage(text = word_to_reply)
 
-    #    line_bot_api.reply_message(
-    #            event.reply_token,
-    #            messages=[text_to_reply])
+        line_bot_api.reply_message(
+                event.reply_token,
+                messages=[text_to_reply])
 
 @handler.add(FollowEvent)
 def RegisRichmenu(event):
