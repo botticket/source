074cf2c58a226985603ccc59792378f282d4bd77
botticket <59758461+botticket@users.noreply.github.com>
2021-03-14 01:46:20 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -91,195 +91,195 @@ def handle_message(event):
     print(request_text)
     linechat(request_text)
 
-    # try:    
-    if 'สวัสดี' in text_from_user:    
-        text_list = ['สวัสดีจ้า คุณ {} สนใจข้อมูลตัวไหนดี'.format(disname),
-                    'สวัสดีจ้า คุณ {} วันนี้จะเล่นตัวไหนดี'.format(disname)]
+    try:    
+        if 'สวัสดี' in text_from_user:    
+            text_list = ['สวัสดีจ้า คุณ {} สนใจข้อมูลตัวไหนดี'.format(disname),
+                        'สวัสดีจ้า คุณ {} วันนี้จะเล่นตัวไหนดี'.format(disname)]
+            from random import choice
+            word_to_reply = choice(text_list)
+            text_to_reply = TextSendMessage(text = word_to_reply)
+            line_bot_api.reply_message(event.reply_token,messages=[text_to_reply])
+            return 'OK'
+
+        else:
+            from bs4 import BeautifulSoup as soup
+            from urllib.request import urlopen as req
+            from pandas_datareader import data
+            from datetime import datetime, date
+            from scipy.stats import linregress
+            import math
+            import numpy as np
+            import pandas as pd  
+
+            code = text_from_user
+            ticket = [text_from_user]
+            symbols = list(map(lambda e: e + '.bk', ticket))
+
+            def checkmarket(code):
+                url = 'https://www.settrade.com/C04_01_stock_quote_p1.jsp?txtSymbol={}&ssoPageId=9&selectPage=1'.format(code)
+                webopen = req(url)
+                page_html = webopen.read()
+                webopen.close()
+
+                data = soup(page_html, 'html.parser')
+                price = data.findAll('div',{'class':'col-xs-6'})
+                title = price[0].text
+                stockprice = price[2].text
+                stockprice = stockprice.replace('\n','')
+                change = price[3].text
+                change = change.replace('\n','')
+                change = change.replace('\r','')
+                change = change[87:]	
+                comvlue = data.findAll('div',{'class':'col-xs-4'})
+                comvlue = comvlue[6].text
+                comvlue = comvlue.replace(',','')
+                comvlue = format(float(comvlue),'')
+                comvluee = format(float(comvlue),',')
+                return [title,stockprice,change,comvlue,comvluee]
+
+            st = checkmarket(code)
+            text_request = '{} {} ({})'.format(st[0], st[1], st[2])
+
+            class stock:
+                def __init__(self,stock):
+                    self.stock = stock
+
+                def ticket(self):
+                    end = datetime.now()
+                    start = datetime(end.year,end.month,end.day)
+                    list = self.stock
+                    
+                    dfall = data.DataReader(f'{list}', data_source="yahoo",start=start_year, end=end)
+
+                    try:
+                        dfY = data.DataReader(f'{list}', data_source="yahoo", start=yearly, end=end)
+                    except ValueError:
+                        dfY = data.DataReader(f'{list}', data_source="yahoo", start=start_year, end=end)
+
+                    try:
+                        dfM = data.DataReader(f'{list}', data_source="yahoo", start=monthly, end=end)
+                    except ValueError:
+                        dfM = data.DataReader(f'{list}', data_source="yahoo", start=start_year, end=end)
+
+                    try:
+                        preM = data.DataReader(f'{list}', data_source="yahoo", start=prevm, end=endvm)
+                    except ValueError:
+                        preM = data.DataReader(f'{list}', data_source="yahoo", start=start_year, end=end)
+
+                    list = list.replace('.bk','') 
+
+                    stock = f'{list}'
+                    dfall.dropna(inplace=True)
+
+                    try:
+                        Close = float(st[1])
+                    except ValueError:
+                        Close = dfall['Close'].iloc[-1]
+                    Close  = '%.2f'%Close
+
+                    Open_all = dfall['Open'].iloc[0]
+                    Open_all  = '%.2f'%Open_all
+
+                    Chg_all = ((float(Close) - float(Open_all))/ float(Open_all))*100
+                    Chg_all = '%.2f'%Chg_all
+
+                    OpenY = dfY['Open'].iloc[0]
+                    OpenY  = '%.2f'%OpenY
+
+                    ChgY = ((float(Close) - float(OpenY)) / float(OpenY) )*100
+                    ChgY = '%.2f'%ChgY
+
+                    OpenM = dfM['Open'].iloc[0]
+                    OpenM  = '%.2f'%OpenM
+
+                    ChgM = ((float(Close) - float(OpenM)) / float(OpenM) )*100
+                    ChgM = '%.2f'%ChgM
+
+                    try:
+                        today_chg = float(st[2])
+                    except ValueError:
+                        today_chg = float(dfall['Close'].iloc[-1]) - float(dfall['Close'].iloc[-2])
+                    today_chg  = '%.2f'%today_chg
+                    
+                    HpreM = preM.nlargest(1, columns='Close')
+                    HpreM = HpreM['Close'].iloc[-1]
+                    HpreM = '%.2f'%HpreM
+
+                    HpreMp = ((float(Close) - float(HpreM))/float(HpreM))*100
+                    HpreMp = '%.2f'%HpreMp
+
+                    min_Y = dfall.nsmallest(1, columns='Close')
+                    min_Y = min_Y['Close'].iloc[-1]
+                    min_Y = '%.2f'%min_Y
+
+                    min_Yp = ((float(min_Y) - float(Close))/float(Close))*100
+                    min_Yp = '%.2f'%min_Yp
+
+                    max_Y = dfall.nlargest(1, columns='High')
+                    max_Y = max_Y['High'].iloc[-1]
+                    max_Y = '%.2f'%max_Y
+
+                    max_Yp = ((float(max_Y) - float(Close))/float(Close))*100
+                    max_Yp = '%.2f'%max_Yp
+                    
+                    dfall['ema'] = dfall['Close'].rolling(35).mean()
+                    ema = dfall['ema'].iloc[-1]
+                    ema = float(ema)
+                    if ema >= 100:
+                        ema = (round(ema/0.5) * 0.5)
+                    elif ema >= 25:
+                        ema = (round(ema/0.25) * 0.25)
+                    elif ema >= 10:
+                        ema = (round(ema/0.1) * 0.1)
+                    elif ema >= 5:
+                        ema = (round(ema/0.05) * 0.05)
+                    else:
+                        ema = (round(ema/0.02) * 0.02)
+                    ema = '%.2f'%ema
+
+                    pema = ((float(Close) - float(ema)) / float(ema))*100
+                    pema = '%.2f'%pema
+
+                    if float(today_chg) >= 0:
+                        if float(Close) > float(OpenY):
+                            if float(Close) >= float(OpenM) :
+                                if float(Close) >= float(ema):
+                                    notice = f'หุ้นเก็บของ {OpenM}'
+                                else:
+                                    notice = f'หุ้นราคาหลุด {ema}'
+                            else:
+                                notice = f'หุ้นมีเท {OpenM}'
+                        elif float(Close) >= float(OpenM) :
+                            if float(Close) >= float(ema):
+                                notice = f'หุ้นมีแรงซื้อ {OpenM}'
+                            else:
+                                notice = f'หุ้นราคาหลุด {ema}'
+                        elif float(Close) >= float(ema):
+                            notice = f'หุ้นกลับตัวเมื่อเลยราคา {ema}'
+                        else:
+                            notice = f'หุ้นหลุด {ema}'
+                    else:
+                        notice = f'กำลังย่อ/ปรับฐาน \nไม่หลุด {ema} ห่อกลับ'
+                        
+                    stocknrequest = st[0]
+                    text_return = f'ตอนนี้ {stocknrequest} \nราคา {Close} ({today_chg}) \n{notice} \n>> แนวต้าน {max_Y}'
+                    word_to_reply = str(text_return)
+                    text_to_reply = TextSendMessage(text = word_to_reply)
+                    line_bot_api.reply_message(event.reply_token,messages=[text_to_reply])
+
+                    text_tobot = f'{list} Y {OpenY} {ChgY}% \ne {ema} ({pema}%) \nH {HpreM} ({HpreMp}%) > {Close} ({today_chg})'
+                    linechat(text_tobot)
+
+            for symbol in symbols:
+                stock(symbol).ticket()
+    except:
+        text_list = ['{} ไม่ส่งข้อมูลกลับมา'.format(text_from_user),
+            '{} หา {} ไม่ถูกต้อง ลองใหม่อีกครั้ง'.format(disname, text_from_user)]
+
         from random import choice
         word_to_reply = choice(text_list)
         text_to_reply = TextSendMessage(text = word_to_reply)
         line_bot_api.reply_message(event.reply_token,messages=[text_to_reply])
-        return 'OK'
-
-    else:
-        from bs4 import BeautifulSoup as soup
-        from urllib.request import urlopen as req
-        from pandas_datareader import data
-        from datetime import datetime, date
-        from scipy.stats import linregress
-        import math
-        import numpy as np
-        import pandas as pd  
-
-        code = text_from_user
-        ticket = [text_from_user]
-        symbols = list(map(lambda e: e + '.bk', ticket))
-
-        def checkmarket(code):
-            url = 'https://www.settrade.com/C04_01_stock_quote_p1.jsp?txtSymbol={}&ssoPageId=9&selectPage=1'.format(code)
-            webopen = req(url)
-            page_html = webopen.read()
-            webopen.close()
-
-            data = soup(page_html, 'html.parser')
-            price = data.findAll('div',{'class':'col-xs-6'})
-            title = price[0].text
-            stockprice = price[2].text
-            stockprice = stockprice.replace('\n','')
-            change = price[3].text
-            change = change.replace('\n','')
-            change = change.replace('\r','')
-            change = change[87:]	
-            comvlue = data.findAll('div',{'class':'col-xs-4'})
-            comvlue = comvlue[6].text
-            comvlue = comvlue.replace(',','')
-            comvlue = format(float(comvlue),'')
-            comvluee = format(float(comvlue),',')
-            return [title,stockprice,change,comvlue,comvluee]
-
-        st = checkmarket(code)
-        text_request = '{} {} ({})'.format(st[0], st[1], st[2])
-
-        class stock:
-            def __init__(self,stock):
-                self.stock = stock
-
-            def ticket(self):
-                end = datetime.now()
-                start = datetime(end.year,end.month,end.day)
-                list = self.stock
-                
-                dfall = data.DataReader(f'{list}', data_source="yahoo",start=start_year, end=end)
-
-                try:
-                    dfY = data.DataReader(f'{list}', data_source="yahoo", start=yearly, end=end)
-                except ValueError:
-                    dfY = data.DataReader(f'{list}', data_source="yahoo", start=start_year, end=end)
-
-                try:
-                    dfM = data.DataReader(f'{list}', data_source="yahoo", start=monthly, end=end)
-                except ValueError:
-                    dfM = data.DataReader(f'{list}', data_source="yahoo", start=start_year, end=end)
-
-                try:
-                    preM = data.DataReader(f'{list}', data_source="yahoo", start=prevm, end=endvm)
-                except ValueError:
-                    preM = data.DataReader(f'{list}', data_source="yahoo", start=start_year, end=end)
-
-                list = list.replace('.bk','') 
-
-                stock = f'{list}'
-                dfall.dropna(inplace=True)
-
-                try:
-                    Close = float(st[1])
-                except ValueError:
-                    Close = dfall['Close'].iloc[-1]
-                Close  = '%.2f'%Close
-
-                Open_all = dfall['Open'].iloc[0]
-                Open_all  = '%.2f'%Open_all
-
-                Chg_all = ((float(Close) - float(Open_all))/ float(Open_all))*100
-                Chg_all = '%.2f'%Chg_all
-
-                OpenY = dfY['Open'].iloc[0]
-                OpenY  = '%.2f'%OpenY
-
-                ChgY = ((float(Close) - float(OpenY)) / float(OpenY) )*100
-                ChgY = '%.2f'%ChgY
-
-                OpenM = dfM['Open'].iloc[0]
-                OpenM  = '%.2f'%OpenM
-
-                ChgM = ((float(Close) - float(OpenM)) / float(OpenM) )*100
-                ChgM = '%.2f'%ChgM
-
-                try:
-                    today_chg = float(st[2])
-                except ValueError:
-                    today_chg = float(dfall['Close'].iloc[-1]) - float(dfall['Close'].iloc[-2])
-                today_chg  = '%.2f'%today_chg
-                
-                HpreM = preM.nlargest(1, columns='Close')
-                HpreM = HpreM['Close'].iloc[-1]
-                HpreM = '%.2f'%HpreM
-
-                HpreMp = ((float(Close) - float(HpreM))/float(HpreM))*100
-                HpreMp = '%.2f'%HpreMp
-
-                min_Y = dfall.nsmallest(1, columns='Close')
-                min_Y = min_Y['Close'].iloc[-1]
-                min_Y = '%.2f'%min_Y
-
-                min_Yp = ((float(min_Y) - float(Close))/float(Close))*100
-                min_Yp = '%.2f'%min_Yp
-
-                max_Y = dfall.nlargest(1, columns='High')
-                max_Y = max_Y['High'].iloc[-1]
-                max_Y = '%.2f'%max_Y
-
-                max_Yp = ((float(max_Y) - float(Close))/float(Close))*100
-                max_Yp = '%.2f'%max_Yp
-                
-                dfall['ema'] = dfall['Close'].rolling(35).mean()
-                ema = dfall['ema'].iloc[-1]
-                ema = float(ema)
-                if ema >= 100:
-                    ema = (round(ema/0.5) * 0.5)
-                elif ema >= 25:
-                    ema = (round(ema/0.25) * 0.25)
-                elif ema >= 10:
-                    ema = (round(ema/0.1) * 0.1)
-                elif ema >= 5:
-                    ema = (round(ema/0.05) * 0.05)
-                else:
-                    ema = (round(ema/0.02) * 0.02)
-                ema = '%.2f'%ema
-
-                pema = ((float(Close) - float(ema)) / float(ema))*100
-                pema = '%.2f'%pema
-
-                if float(today_chg) >= 0:
-                    if float(Close) > float(OpenY):
-                        if float(Close) >= float(OpenM) :
-                            if float(Close) >= float(ema):
-                                notice = f'หุ้นเก็บของ {OpenM}'
-                            else:
-                                notice = f'หุ้นราคาหลุด {ema}'
-                        else:
-                            notice = f'หุ้นมีเท {OpenM}'
-                    elif float(Close) >= float(OpenM) :
-                        if float(Close) >= float(ema):
-                            notice = f'หุ้นมีแรงซื้อ {OpenM}'
-                        else:
-                            notice = f'หุ้นราคาหลุด {ema}'
-                    elif float(Close) >= float(ema):
-                        notice = f'หุ้นกลับตัวเมื่อเลยราคา {ema}'
-                    else:
-                        notice = f'หุ้นหลุด {ema}'
-                else:
-                    notice = f'กำลังย่อ/ปรับฐาน \nไม่หลุด {ema} ห่อกลับ'
-                    
-                stocknrequest = st[0]
-                text_return = f'ตอนนี้ {stocknrequest} \nราคา {Close} ({today_chg}) \n{notice} \n>> แนวต้าน {max_Y}'
-                word_to_reply = str(text_return)
-                text_to_reply = TextSendMessage(text = word_to_reply)
-                line_bot_api.reply_message(event.reply_token,messages=[text_to_reply])
-
-                text_tobot = f'{list} Y {OpenY} {trendAll} {ChgY}% \ne {ema} ({pema}%) \nH {HpreM} ({HpreMp}%) > {Close} ({today_chg})'
-                linechat(text_tobot)
-
-        for symbol in symbols:
-            stock(symbol).ticket()
-    # except:
-    #     text_list = ['{} ไม่มีในฐานข้อมูล {} ลองใหม่อีกครั้ง'.format(text_from_user,disname),
-    #         '{} ค้นหาหุ้น {} ไม่ถูกต้อง ลองใหม่อีกครั้ง'.format(disname, text_from_user)]
-
-    #     from random import choice
-    #     word_to_reply = choice(text_list)
-    #     text_to_reply = TextSendMessage(text = word_to_reply)
-    #     line_bot_api.reply_message(event.reply_token,messages=[text_to_reply])
 
 @handler.add(FollowEvent)
 def RegisRichmenu(event):
